b12786a209d5aebe9bb85ae170b83a431d10e7b1
flashingstars <annkarye@gmail.com>
2023-07-27 11:49:12 +0300

    Reacting app

--- 0x01-react_intro/task_2/dashboard/src/App.js
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -10,9 +10,12 @@ function App() {
         <h1> School dashboard </h1>
       </div>
       <div className="App-body">
-        <p>
-          Login to access the full dashboard
-        </p>
+        <p>Login to access the full dashboard</p>
+        <label htmlfor="email">Email: </label>
+        <input type="email" name="email" id="email" />
+        <label htmlfor="password">Password: </label>
+        <input type="password" name="password" id="password" />
+        <button>OK</button>
       </div>
       <div className="App-footer">
         <p>
