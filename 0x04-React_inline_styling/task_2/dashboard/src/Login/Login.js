066aea61e03b55e89b3a66eb23ea159ab3f0ef80
flashingstars <annkarye@gmail.com>
2023-08-09 18:01:03 +0300

    Inline styling

---
+++ 0x04-React_inline_styling/task_2/dashboard/src/Login/Login.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { StyleSheet, css } from "aphrodite";
+
+function Login() {
+    const styles = StyleSheet.create({
+        App: {
+            textAlign: 'center',
+            fontFamily: 'Arial, sans-serif',
+        },
+        AppBody: {
+            margin: '50px',
+            textAlign: 'left',
+        },
+        input: {
+            marginRight: '10px',
+        }
+    });
+    return (
+        <div className={css(styles.App)}>
+            <div className={css(styles.AppBody)}>
+                <p>Login to access the full dashboard</p>
+                <label htmlFor="email">Email: </label>
+                <input type="email" name="email" id="email" />
+                <label htmlFor="password">Password: </label>
+                <input type="password" name="password" id="password" />
+                <button>OK</button>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
