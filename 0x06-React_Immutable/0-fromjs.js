fe4af6e29b4b2743c3123c57fa41481396f1413a
flashingstars <annkarye@gmail.com>
2023-08-17 16:22:26 +0300

    Converting into an immutable Map

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,6 +1,10 @@
 import { fromJS } from 'immutable';
 
 function getImmutableObject(object) {
+    if (typeof object !== 'object' || object === null) {
+        throw new Error('object must be an object')
+    };
+
     return fromJS(object);
 }
 
